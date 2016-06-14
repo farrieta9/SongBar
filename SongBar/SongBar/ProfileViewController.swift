4bbe15f5c7f8ef04fa559313bace6176c96cd9c8
farrieta9 <francisco.arrieta@aggiemail.usu.edu>
2016-06-13 22:42:49 -0600

    Started creating the user profile view controller

---
+++ SongBar/SongBar/ProfileViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ProfileViewController.swift
+//  SongBar
+//
+//  Created by Francisco Arrieta on 6/13/16.
+//  Copyright © 2016 lil9porkchop. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension ProfileViewController: UITableViewDataSource {
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 1
+	}
+	func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+		return 1
+	}
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+		cell.detailTextLabel?.text = "some text"
+		return cell
+	}
+}
