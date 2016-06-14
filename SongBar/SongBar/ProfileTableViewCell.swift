b530ae067f49586910e187e2ecc39a76bccf8cf2
farrieta9 <francisco.arrieta@aggiemail.usu.edu>
2016-06-14 16:51:33 -0600

    Added images

---
+++ SongBar/SongBar/ProfileTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileTableViewCell.swift
+//  SongBar
+//
+//  Created by Francisco Arrieta on 6/14/16.
+//  Copyright © 2016 lil9porkchop. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
