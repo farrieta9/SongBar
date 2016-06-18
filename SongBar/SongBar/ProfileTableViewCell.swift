27246cb62ac8db719561d91648f7046e66f9a1c2
farrieta9 <francisco.arrieta@aggiemail.usu.edu>
2016-06-18 16:54:30 -0600

    Got rid of the previous profile controller. see previous commits for that code

--- SongBar/SongBar/ProfileTableViewCell.swift
+++ SongBar/SongBar/ProfileTableViewCell.swift
@@ -1,18 +1,41 @@
 //
-//  ProfileTableViewCell.swift
+//  Profile2TableViewCell.swift
 //  SongBar
 //
-//  Created by Francisco Arrieta on 6/14/16.
+//  Created by Francisco Arrieta on 6/18/16.
 //  Copyright © 2016 lil9porkchop. All rights reserved.
 //
 
 import UIKit
 
+
 class ProfileTableViewCell: UITableViewCell {
 
+	@IBOutlet weak var userImageView: UIImageView!
+	@IBOutlet weak var userNameLabel: UILabel!
+	@IBOutlet weak var actionButton: UIButton!
+	
+	var username = "" {
+		didSet {
+			userNameLabel.text = username
+		}
+	}
+	
+	var userImage = UIImage(named: "default_profile.png") {
+		didSet {
+			userImageView.image = userImage
+		}
+	}
+	
+	
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+		actionButton.setTitle("Follow", forState: .Normal)
+		actionButton.backgroundColor = UIColor.clearColor()
+		actionButton.layer.cornerRadius = 15
+		actionButton.layer.borderWidth = 1
+		actionButton.layer.borderColor = UIColor.blackColor().CGColor
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
@@ -20,5 +43,6 @@ class ProfileTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+
 
 }
--- SongBar/SongBar/ProfileViewController.swift
+++ SongBar/SongBar/ProfileViewController.swift
@@ -1,123 +1,95 @@
 //
-//  LoginViewController.swift
+//  Profile2ViewController.swift
 //  SongBar
 //
-//  Created by Francisco Arrieta on 6/13/16.
+//  Created by Francisco Arrieta on 6/18/16.
 //  Copyright © 2016 lil9porkchop. All rights reserved.
 //
-
 
 import UIKit
 import Firebase
 
-let offset_HeaderStop:CGFloat = 40.0 // At this offset the Header stops its transformations
-let distance_W_LabelHeader:CGFloat = 30.0 // The distance between the top of the screen and the top of the White Label
-
-
-enum contentTypes {
-	case Audience, Follow
-}
-
 class ProfileViewController: UIViewController {
 	
-	// MARK: Outlet properties
 	
-	@IBOutlet var tableView : UITableView!
-	@IBOutlet var headerView : UIView!
-	@IBOutlet var profileView : UIView!
-	@IBOutlet var segmentedView : UIView!
-	@IBOutlet var avatarImage:UIImageView!
-	@IBOutlet var handleLabel : UILabel!
-	@IBOutlet var headerLabel : UILabel!
-	@IBOutlet weak var userTagLabel: UILabel!
+	@IBOutlet weak var tableView: UITableView!
+	private let tableHeaderHeight: CGFloat = 100.0
+	private let tableHeaderCutAway: CGFloat = 0.0
+	
+	private var headerView: ProfileHeaderView!
+	private var headerMaskLayer: CAShapeLayer!
+	
+	enum contentTypes {
+		case Audience, Follow
+	}
+	
+	var contentToDisplay: contentTypes = .Audience
 	var audienceData = [String]()
 	var followData = [String]()
-	
-	var data = ["iPad", "iPhone", "iWatch", "iPod", "iMac"]
-	var buttonData = ["US","China","London","Canada","Japan"];
-	
-	
-	var headerImageView:UIImageView!
-	var contentToDisplay: contentTypes = .Audience
-	let userDefaults = NSUserDefaults.standardUserDefaults()
-	
-	let actionButtonWidth: CGFloat = 100.0
-	let actionButtonHeight:CGFloat = 30.0
-	
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		tableView.contentInset = UIEdgeInsetsMake(headerView.frame.height, 0, 0, 0)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
 		
+		headerView = tableView.tableHeaderView as! ProfileHeaderView
+		tableView.tableHeaderView = nil  // Clear out the default header
+		tableView.addSubview(headerView) // Add header
+		
+		tableView.contentInset = UIEdgeInsets(top: tableHeaderHeight, left: 0, bottom: 0, right: 0)
+		tableView.contentOffset = CGPoint(x: 0, y: -tableHeaderHeight)
+		
+		headerMaskLayer = CAShapeLayer()
+		headerMaskLayer.fillColor = UIColor.blackColor().CGColor
+		
+		headerView.layer.mask = headerMaskLayer
+		
+		updateHeaderView()
+		
+		refreshAudience()
+		refreshFollow()
+		
+    }
+	
+	func refreshAudience() {
 		// Get the audience
 		FIRDatabase.database().reference().child("users/users_by_name/\(Utilities.getCurrentUsername())/audience_by_id").observeEventType(.Value, withBlock: {(snapshot) in
-//			print(snapshot)
+			
 			guard let results = snapshot.value as? [String: [String: String]] else {
 				print("getting the audience failed")
 				return
 			}
 			
-//			print(results)
 			for person in results.keys {
 				self.audienceData.append(person)
 			}
-//			print(self.audienceData)
 			
 			self.tableView.reloadData()
 		})
-		
+	}
+	
+	func refreshFollow() {
 		FIRDatabase.database().reference().child("users/users_by_name/\(Utilities.getCurrentUsername())/friends_by_id").observeEventType(.Value, withBlock: {(snapshot) in
-		
-			print(snapshot)
+			
 			guard let results = snapshot.value as? [String: [String: String]] else {
 				print("getting friends failed") // Perhaps there are no results
 				return
 			}
-			print(results)
 			for person in results.keys {
 				self.followData.append(person)
 			}
 			self.tableView.reloadData()
 		})
-		
 	}
 	
-	override func viewDidAppear(animated: Bool) {
-		
-		// Header - Image
-		
-		headerImageView = UIImageView(frame: headerView.bounds)
-		headerImageView?.image = UIImage(named: "Music-Concert-Crowd")
-		headerImageView?.contentMode = UIViewContentMode.ScaleAspectFill
-		headerView.insertSubview(headerImageView, belowSubview: headerLabel)
-		headerView.clipsToBounds = true
-		
-		userTagLabel.text = FIRAuth.auth()?.currentUser?.displayName  // TODO: Have the userTag have a @ at the beginning
-		
+	override func viewWillLayoutSubviews() {
+		super.viewWillLayoutSubviews()
+		updateHeaderView()
 	}
 	
-	@IBAction func onSignOut(sender: UIButton) {
-		print("Sign out")
-		do {
-			try	FIRAuth.auth()?.signOut()
-		} catch let error {
-			print(error)
-		}
-		resetCurrentUser()
-		
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		updateHeaderView()
 	}
-	
-	func resetCurrentUser() {
-		userDefaults.setValue("", forKey: "email")
-		userDefaults.setValue("", forKey: "password")
-	}
-	
-	// MARK: Table view processing
-	
-	
-	
-	@IBAction func onSelectContentType(sender: UISegmentedControl) {
-		
+	@IBAction func onContentDisplayChange(sender: UISegmentedControl) {
 		switch sender.selectedSegmentIndex {
 		case 0:
 			contentToDisplay = .Audience
@@ -126,204 +98,85 @@ class ProfileViewController: UIViewController {
 		default:
 			break
 		}
-		
 		tableView.reloadData()
 	}
 	
-	@IBAction func shamelessActionThatNowBringsYouToDeansTwitterProfile() {
+	func updateHeaderView() {
+		let effectiveHeight = tableHeaderHeight - tableHeaderCutAway / 2
+		var headerRect = CGRect(x: 0, y: -effectiveHeight, width: tableView.bounds.width, height: tableHeaderHeight)
 		
-//		if !UIApplication.sharedApplication().openURL(NSURL(string:"twitter://user?screen_name=deanbrindley87")!){
-//			UIApplication.sharedApplication().openURL(NSURL(string:"https://twitter.com/deanbrindley87")!)
-//		}
-		print("Follow button pressed")
+		if tableView.contentOffset.y < -effectiveHeight {
+			headerRect.origin.y = tableView.contentOffset.y
+			headerRect.size.height = -tableView.contentOffset.y + tableHeaderCutAway / 2
+		}
+		
+		headerView.frame = headerRect
+		
+		// Cut away
+		let path = UIBezierPath()
+		path.moveToPoint(CGPoint(x: 0, y: 0))
+		path.addLineToPoint(CGPoint(x: headerRect.width, y: 0))
+		path.addLineToPoint(CGPoint(x: headerRect.width, y: headerRect.height))
+		
+		path.addLineToPoint(CGPoint(x: 0, y:headerRect.height - tableHeaderCutAway))
+		
+		headerMaskLayer?.path = path.CGPath
 	}
-	
-	//Button Action is
-	func buttonPressed(sender:UIButton!)
-	{
-		print("oh yeha")
-		let buttonRow = sender.tag
-		print("button is Pressed")
-		print("Clicked Button Row is", buttonRow)
-	}
-	
 }
 
 
-
-extension ProfileViewController: UITableViewDataSource {
+extension ProfileViewController: UITableViewDataSource	{
 	func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+		return 2
+	}
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		if section == 1 {
+			switch contentToDisplay {
+			case .Audience:
+				return audienceData.count
+				
+			case .Follow:
+				return followData.count
+			}
+		}
 		return 1
 	}
 	
-	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		
-		switch contentToDisplay {
-		case .Audience:
-			return audienceData.count
-			
-		case .Follow:
-			return followData.count
-		}
-		
-	}
-	
 	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-		let cell = UITableViewCell()
-
 		
-		switch contentToDisplay {
-		case .Audience:
-			cell.textLabel?.text = audienceData[indexPath.row]
+		if indexPath.section == 1 {
+			let cell = tableView.dequeueReusableCellWithIdentifier("cell") as! ProfileTableViewCell
 			
-		case .Follow:
-			cell.imageView?.image = UIImage(named: "default_profile.png")
-			cell.textLabel?.text = followData[indexPath.row]
+			switch contentToDisplay {
+			case .Audience:
+				cell.username = audienceData[indexPath.row]
+			case .Follow:
+				cell.username = followData[indexPath.row]
+			}
+			
+			return cell
+		} else {
+			let cell = tableView.dequeueReusableCellWithIdentifier("headerCell") as! ProfileHeaderTableViewCell
+			cell.username = Utilities.getCurrentUsername()
+			
+			return cell
 		}
-		
-		return cell
-		
-//		let label = UILabel(frame: CGRectMake(280.0, 14.0, 100.0, 30.0))
-//		label.text = data[indexPath.row]
-//		label.tag = indexPath.row
-//		cell.contentView.addSubview(label)
-//		
-//		let btn = UIButton(type: UIButtonType.Custom) as UIButton
-//		btn.backgroundColor = UIColor.greenColor()
-//		btn.setTitle(buttonData[indexPath.row], forState: UIControlState.Normal)
-//		btn.frame = CGRectMake(20, 5, 30, 30)
-//		btn.addTarget(self, action: #selector(buttonPressed(_:)), forControlEvents: .TouchUpInside)
-//		btn.tag = indexPath.row
-//		cell.contentView.addSubview(btn)
-		
-//		let usernameLabel = UILabel(frame: CGRectMake(75, 5, 100, 30))
-//		let actionButton = UIButton(type: .Custom)
-//		
-//		actionButton.frame = CGRectMake(tableView.frame.width - actionButtonWidth - 15, 5, actionButtonWidth, actionButtonHeight)
-//
-//		usernameLabel.tag = indexPath.row
-//		actionButton.tag = indexPath.row
-//		
-//		actionButton.setTitle("Follow", forState: .Normal)
-//		
-//		
-//		switch contentToDisplay {
-//		case .Audience:
-//			usernameLabel.text = audienceData[indexPath.row]
-//		case .Follow:
-//			usernameLabel.text = followData[indexPath.row]
-//		}
-//		actionButton.layer.cornerRadius = 15
-//		actionButton.layer.borderWidth = 1
-//		actionButton.layer.borderColor = UIColor.blackColor().CGColor
-//		actionButton.backgroundColor = UIColor.blueColor()
-//		
-//		cell.contentView.addSubview(usernameLabel)
-//		cell.contentView.addSubview(actionButton)
-//		
-//		return cell
 	}
-	
+	func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+		
+		switch indexPath.section {
+		case 0:
+			return 175
+		case 1:
+			return 50
+		default:
+			return 50
+		}
+	}
 }
 
 extension ProfileViewController: UIScrollViewDelegate {
-	// MARK: Scroll view delegate
-	
 	func scrollViewDidScroll(scrollView: UIScrollView) {
-		
-		let offset = scrollView.contentOffset.y + headerView.bounds.height
-		
-		var avatarTransform = CATransform3DIdentity
-		var headerTransform = CATransform3DIdentity
-		
-		// PULL DOWN -----------------
-		
-		if offset < 0 {
-			
-			let headerScaleFactor:CGFloat = -(offset) / headerView.bounds.height
-			let headerSizevariation = ((headerView.bounds.height * (1.0 + headerScaleFactor)) - headerView.bounds.height)/2
-			headerTransform = CATransform3DTranslate(headerTransform, 0, headerSizevariation, 0)
-			headerTransform = CATransform3DScale(headerTransform, 1.0 + headerScaleFactor, 1.0 + headerScaleFactor, 0)
-			
-			
-			// Hide views if scrolled super fast
-			headerView.layer.zPosition = 0
-			headerLabel.hidden = true
-			
-		}
-			
-			// SCROLL UP/DOWN ------------
-			
-		else {
-			
-			// Header -----------
-			
-			headerTransform = CATransform3DTranslate(headerTransform, 0, max(-offset_HeaderStop, -offset), 0)
-			
-			//  ------------ Label
-			
-			headerLabel.hidden = false
-			let alignToNameLabel = -offset + handleLabel.frame.origin.y + headerView.frame.height + offset_HeaderStop
-			
-			headerLabel.frame.origin = CGPointMake(headerLabel.frame.origin.x, max(alignToNameLabel, distance_W_LabelHeader + offset_HeaderStop))
-			
-			// Avatar -----------
-			
-			let avatarScaleFactor = (min(offset_HeaderStop, offset)) / avatarImage.bounds.height / 1.4 // Slow down the animation
-			let avatarSizeVariation = ((avatarImage.bounds.height * (1.0 + avatarScaleFactor)) - avatarImage.bounds.height) / 2.0
-			avatarTransform = CATransform3DTranslate(avatarTransform, 0, avatarSizeVariation, 0)
-			avatarTransform = CATransform3DScale(avatarTransform, 1.0 - avatarScaleFactor, 1.0 - avatarScaleFactor, 0)
-			
-			if offset <= offset_HeaderStop {
-				
-				if avatarImage.layer.zPosition < headerView.layer.zPosition{
-					headerView.layer.zPosition = 0
-					headerImageView.alpha = 1.0
-				}
-				
-				
-			}else {
-				if avatarImage.layer.zPosition >= headerView.layer.zPosition{
-					headerView.layer.zPosition = 2
-					headerImageView.alpha = 0.5
-				}
-			}
-		}
-		
-		// Apply Transformations
-		headerView.layer.transform = headerTransform
-		avatarImage.layer.transform = avatarTransform
-		
-		// Segment control
-		
-		let segmentViewOffset = profileView.frame.height - segmentedView.frame.height - offset
-		
-		var segmentTransform = CATransform3DIdentity
-		
-		// Scroll the segment view until its offset reaches the same offset at which the header stopped shrinking
-		segmentTransform = CATransform3DTranslate(segmentTransform, 0, max(segmentViewOffset, -offset_HeaderStop), 0)
-		
-		segmentedView.layer.transform = segmentTransform
-		
-		
-		// Set scroll view insets just underneath the segment control
-		tableView.scrollIndicatorInsets = UIEdgeInsetsMake(segmentedView.frame.maxY, 0, 0, 0)
-		
-		
-		
+		updateHeaderView()
 	}
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
